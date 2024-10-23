1d008de93822315921b8122467400513c080bf57
Simon Inns <simoninns@fastmail.com>
2024-10-23 19:17:53 +0200

    Partial check in of communicator firmware since I need to update the kicad

---
+++ communicator/firmware/usb_cdc.h
@@ -0,0 +1,30 @@
+/************************************************************************ 
+
+    usb_cdc.h
+
+    Valiant Turtle 2 Communicator - Raspberry Pi Pico W Firmware
+    Copyright (C) 2024 Simon Inns
+
+    This file is part of Valiant Turtle 2
+
+    This is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
+#ifndef USB_CDC_H_
+#define USB_CDC_H_
+
+#endif /* USB_CDC_H_ */
