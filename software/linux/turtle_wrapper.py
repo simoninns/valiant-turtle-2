a16f764c794460526d1e60b63ea370b8ab4670f5
Simon Inns <simoninns@fastmail.com>
2024-12-31 19:56:36 +0100

    Improved the demo and made it easier to see it on the screen as well as the robot

---
+++ software/linux/turtle_wrapper.py
@@ -0,0 +1,94 @@
+#************************************************************************ 
+#
+#   turtle_wrapper.py
+#
+#   A turtle wrapper class to simplify the turtle commands
+#   Valiant Turtle 2 - Communicator Linux Firmware
+#   Copyright (C) 2024 Simon Inns
+#
+#   This file is part of Valiant Turtle 2
+#
+#   This is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   Email: simon.inns@gmail.com
+#
+#************************************************************************
+
+from commands_tx import CommandsTx
+import time
+
+class TurtleWrapper:
+    def __init__(self):
+        self._commands_tx = CommandsTx()
+        self._pen_down = False
+        self._angle = 0
+        self._x = 0
+        self._y = 0
+
+    def connect(self):
+        self._commands_tx.connect()
+        while not self._commands_tx.connected:
+            time.sleep(1)
+        self._commands_tx.reset_origin()
+        self._commands_tx.eyes(0, 0, 0, 255)
+
+    def disconnect(self):
+        self._commands_tx.eyes(0, 0, 0, 0)
+        self._commands_tx.disconnect()
+
+    def penup(self):
+        self._pen_down = False
+        self._commands_tx.eyes(0, 0, 255, 0)
+        self._commands_tx.pen(True)
+
+    def pendown(self):
+        self._pen_down = True
+        self._commands_tx.eyes(0, 255, 0, 0)
+        self._commands_tx.pen(False)
+
+    def goto(self, x, y):
+        self._commands_tx.position(x, y)
+
+    def forward(self, distance):
+        self._commands_tx.forward(distance)
+
+    def backward(self, distance):
+        self._commands_tx.backward(distance)
+
+    def setheading(self, angle):
+        self._angle = angle % 360
+        self._commands_tx.heading(self._angle)
+
+    def right(self, angle):
+        self._commands_tx.right(angle)
+
+    def left(self, angle):
+        self._commands_tx.left(angle)
+
+    def circle(self, radius, extent=360):
+        self._commands_tx.circle(radius, extent)
+
+    def position(self):
+        _, self._x, self._y = self._commands_tx.get_position()
+        return self._x, self._y
+
+    def heading(self):
+        _, self._angle = self._commands_tx.get_heading()
+        return self._angle
+
+    def motors_on(self):
+        self._commands_tx.motors(True)
+
+    def motors_off(self):
+        self._commands_tx.motors(False)
--- software/linux/vt2_demo.py
+++ software/linux/vt2_demo.py
@@ -25,123 +25,125 @@
 #
 #************************************************************************
 
+import math
 import logging
-from commands_tx import CommandsTx
-import sys
 import time
-import cmd
+import sys
+from turtle_wrapper import TurtleWrapper
+import turtle
 
-def draw_simple_cat(commands_tx: CommandsTx):
-    commands_tx.motors(True)
-
+def draw_simple_cat(turtle):
     # Draw the head (circle)
-    commands_tx.pen(True)
-    commands_tx.position(0, 50)
-    commands_tx.pen(False)
-    commands_tx.circle(50, 360)
+    turtle.penup()
+    turtle.goto(0, 50)
+    turtle.pendown()
+    turtle.circle(50, 360)
 
     # Draw the left ear (triangle pointing upwards)
-    commands_tx.pen(True)
-    commands_tx.position(-40, 150+30)
-    commands_tx.pen(False)
-    commands_tx.position(-50, 100+30)
-    commands_tx.position(-10, 100+30)
-    commands_tx.position(-40, 150+30)
+    turtle.penup()
+    turtle.goto(-40, 150 + 30)
+    turtle.pendown()
+    turtle.goto(-50, 100 + 30)
+    turtle.goto(-10, 100 + 30)
+    turtle.goto(-40, 150 + 30)
 
     # Draw the right ear (triangle pointing upwards)
-    commands_tx.pen(True)
-    commands_tx.position(40, 150+30)
-    commands_tx.pen(False)
-    commands_tx.position(50, 100+30)
-    commands_tx.position(10, 100+30)
-    commands_tx.position(40, 150+30)
+    turtle.penup()
+    turtle.goto(40, 150 + 30)
+    turtle.pendown()
+    turtle.goto(50, 100 + 30)
+    turtle.goto(10, 100 + 30)
+    turtle.goto(40, 150 + 30)
 
     # Draw the eyes (small circles)
-    commands_tx.pen(True)
-    commands_tx.position(-20, 100)
-    commands_tx.pen(False)
-    commands_tx.circle(10, 360)
-    commands_tx.pen(True)
-    commands_tx.position(20, 100)
-    commands_tx.pen(False)
-    commands_tx.circle(10, 360)
+    turtle.penup()
+    turtle.goto(-20, 100)
+    turtle.pendown()
+    turtle.circle(10, 360)
+    turtle.penup()
+    turtle.goto(20, 100)
+    turtle.pendown()
+    turtle.circle(10, 360)
 
     # Draw the nose (triangle)
-    commands_tx.pen(True)
-    commands_tx.position(0, 90)
-    commands_tx.pen(False)
-    commands_tx.position(-10, 80)
-    commands_tx.position(10, 80)
-    commands_tx.position(0, 90)
+    turtle.penup()
+    turtle.goto(0, 90)
+    turtle.pendown()
+    turtle.goto(-10, 80)
+    turtle.goto(10, 80)
+    turtle.goto(0, 90)
 
     # Draw the mouth (lines)
-    commands_tx.pen(True)
-    commands_tx.position(0, 80)
-    commands_tx.heading(315)
-    commands_tx.pen(False)
-    commands_tx.forward(15)
-    commands_tx.pen(True)
-    commands_tx.position(0, 80)
-    commands_tx.heading(225)
-    commands_tx.pen(False)
-    commands_tx.forward(15)
+    turtle.penup()
+    turtle.goto(0, 80)
+    turtle.setheading(315)
+    turtle.pendown()
+    turtle.forward(15)
+    turtle.penup()
+    turtle.goto(0, 80)
+    turtle.setheading(225)
+    turtle.pendown()
+    turtle.forward(15)
 
     # Draw the whiskers (lines)
-    # Left side
     for angle in [200, 180, 160]:
-        commands_tx.pen(True)
-        commands_tx.position(-10, 85)
-        commands_tx.heading(angle)
-        commands_tx.pen(False)
-        commands_tx.forward(60)
+        turtle.penup()
+        turtle.goto(-10, 85)
+        turtle.setheading(angle)
+        turtle.pendown()
+        turtle.forward(60)
 
-    # Right side
     for angle in [340, 0, 20]:
-        commands_tx.pen(True)
-        commands_tx.position(10, 85)
-        commands_tx.heading(angle)
-        commands_tx.pen(False)
-        commands_tx.forward(60)
+        turtle.penup()
+        turtle.goto(10, 85)
+        turtle.setheading(angle)
+        turtle.pendown()
+        turtle.forward(60)
 
     # Draw the body (ellipse-like shape)
-    commands_tx.pen(True)
-    commands_tx.position(0, 0)
-    commands_tx.pen(False)
-    commands_tx.heading(270)
+    turtle.penup()
+    turtle.goto(0, 0)
+    turtle.pendown()
+    turtle.setheading(270)
     for _ in range(2):
-        commands_tx.circle(50, 90)
-        commands_tx.circle(100, 90)
+        turtle.circle(50, 90)
+        turtle.circle(100, 90)
 
     # Draw the tail (curved line)
-    commands_tx.pen(True)
-    commands_tx.position(50, -50)
-    commands_tx.heading(120)
-    commands_tx.pen(False)
-    commands_tx.circle(40, 200)
+    turtle.penup()
+    turtle.goto(50, -50)
+    turtle.setheading(120)
+    turtle.pendown()
+    turtle.circle(40, 200)
 
-    commands_tx.pen(True)
-    commands_tx.motors(False)
+    turtle.penup()
 
-def main():
-    # Configure the logging module
-    log_format = "[%(asctime)s %(filename)s::%(funcName)s():%(lineno)s]%(levelname)s: %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_format, filename="vt2_cmdtest.log")
+# Call this to use the real robot
+def main_physical():
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+    turtle = TurtleWrapper()
 
-    commands_tx = CommandsTx()
+    try:
+        turtle.connect()
+        logging.info("Connected to BLE")
+        turtle.motors_on()
+        draw_simple_cat(turtle)
+        turtle.motors_off()
+        logging.info("Drawing completed")
+    finally:
+        turtle.disconnect()
+        logging.info("Disconnected from BLE")
 
-    # Wait for BLE connection
-    commands_tx.connect()
-    logging.info("Waiting for BLE connection...")
-    while not commands_tx.connected:
-        time.sleep(1)
-    logging.info("Connected to BLE")
+# Call this to use the screen turtle graphics class
+def main_screen():
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
 
-    draw_simple_cat(commands_tx)
-    logging.info("Command tests completed. Exiting program.")
-    commands_tx.disconnect()
-    logging.info("Disconnected from BLE")
+    turtle.speed(1)
+    draw_simple_cat(turtle)
+    logging.info("Drawing completed")
+    turtle.goto(0,0)
+    turtle.setheading(0)
+    turtle.done()
 
-    sys.exit(0)
-
-if __name__ == "__main__":
-    main()
+#main_screen()
+main_physical()
